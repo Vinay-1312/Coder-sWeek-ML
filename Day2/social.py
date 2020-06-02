8881417faf7c4f774d7f3bab5f85e9c33178b608
Vinay-1312 <66013055+Vinay-1312@users.noreply.github.com>
2020-06-02 17:56:31 +0530

    Add files via upload

---
+++ Day2/social.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 14:03:43 2020
+
+@author: dell
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+data=pd.read_csv("Social_networking_ads.csv")
+
+y=data["Purchased"].values
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+data['Gender'] = le.fit_transform(data['Gender'])
+x=data[["Age","EstimatedSalary"]].values
+corr = data.corr()
+f, ax = plt.subplots(figsize=(8, 8))
+sns.heatmap(corr,square=True,vmax=3,annot=True)
+plt.show()
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=0)
+from sklearn.tree import DecisionTreeClassifier
+dt=DecisionTreeClassifier()
+clf=dt.fit(x_train,y_train)
+ypred_train=dt.predict(x_train)
+ypred=dt.predict(x_test)
+accuracy=accuracy_score(ypred,y_test)
+
+print("accuracy socre",accuracy*100)
+plt.plot(x_train,ypred_train,"bo")
